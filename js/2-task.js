828b71882ef1bcb5b95283fdc14a00805d6dbc4d
Mokalol25 <marko.zhoba@gmail.com>
2023-12-18 17:47:12 +0200

    13-hw-js fixed

--- js/2-task.js
+++ js/2-task.js
@@ -2,9 +2,12 @@ const weather = {
     temperature: prompt("введіть температуру"),
     humidity: 10,
     windSpeed: 2,
+    checkTemperature() {
+        if (weather.temperature > 0) {
+            console.log("false")
+        } else {
+            console.log("true")
+        }
+    }
 }
-if (weather.temperature > 0) {
-    console.log("false")
-} else {
-    console.log("true")
-}
+weather.checkTemperature()
--- js/3-task.js
+++ js/3-task.js
@@ -2,16 +2,22 @@ const user = {
     name: 'Marko',
     email: '1234@gmail.com',
     password: 1234,
-}
-const checkEmail = prompt("Введіть email:")
+    emailCheck() {
+        const checkEmail = prompt("Введіть email:")
     if (checkEmail === user.email) {
         alert("Правильно")
     } else {
         alert("error")
     }
-const checkPassword = Number(prompt("Введіть password:"))
+    },
+    passwordCheck() {
+        const checkPassword = Number(prompt("Введіть password:"))
     if (checkPassword === user.password) {
     alert("Правильно")
 } else {
     alert("error")
 }
+    }
+}
+user.emailCheck()
+user.passwordCheck()
